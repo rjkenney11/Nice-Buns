c25ade349ee986b598902c20d7ce8420ff2d1809
rjkenney11 <rachelkenney@Rachels-MBP.fios-router.home>
2021-03-23 19:18:16 -0400

    routes and base

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Summer Thyme Burger');
+INSERT INTO burgers (burger_name) VALUES ('Poblano Picasso Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chard to a Crisp Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fig-eta Bout it Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Kale Mary Burger', true);
