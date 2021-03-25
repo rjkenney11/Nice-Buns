0542c1d861069d9918b37d3e636da0bdd6ac0fd7
rjkenney11 <rachelkenney@Rachels-MacBook-Pro.local>
2021-03-25 10:14:32 -0400

    didn't save

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -35,7 +35,9 @@ router.put("/api/burgers/:id", function(req, res) {
   // console.log("my body ", req.params.body);
   console.log("condition", condition);
 
-  burger.updateOne({ devoured: req.body.devoured}, condition, function(result) {
+  burger.updateOne(
+    { devoured: req.body.devoured},
+     condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
